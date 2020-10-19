3d135a6cd6290b7cdb6febf9c008120200de8e1b
JinaStrickland <Jina30@gmail.com>
2020-10-19 16:34:17 -0400

    made fist fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const url = ""
+const postUrl = "http://localhost:3000/posts/"
 
 document.addEventListener("DOMContentLoaded", () => {
+    fetchPosts()
 
 })
+
+function fetchPosts() {
+
+    const ul = document.querySelector("#post-text")
+    fetch(postUrl)
+    .then(res => res.json())
+    .then(posts => posts.forEach(post => displayPosts(post)))
+
+}
+
+function displayPosts(post) {
+    const ul = document.querySelector("#post-text")
+    const li = document.createElement("li")
+    li.innerHTML = post.post
+    ul.append(li)
+}
