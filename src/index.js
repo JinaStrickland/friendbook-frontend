b7c5fd2f25ded13397484f67ef9bc83bcaabf9bf
tcharfi66 <charfitej6@gmail.com>
2020-10-20 11:53:25 -0400

    Show Page for user

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const postUrl = "http://localhost:3000/posts/"
+const userURL = "http://localhost:3000/users/"
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetchPosts()
+    //fetchPosts()
+    fetchUser()
     
 })
 
 function fetchPosts() {
-
     const ul = document.querySelector("#post-text")
     fetch(postUrl)
     .then(res => res.json())
@@ -14,12 +15,24 @@ function fetchPosts() {
 
 }
 
+function fetchUser() {
+    fetch(userURL+"/22")
+    .then(res => res.json())
+    .then(printPosts)
+}
+
+function printPosts(user){
+  user.posts.forEach(post => displayPosts(post))
+  const username = document.querySelector("#username")
+  username.innerText = user.name
+}
+
 function displayPosts(post) {
     const ul = document.querySelector("#post-text")
     const p = document.createElement("p")
-    p.innerHTML = post.post + post.user_id
+    p.innerHTML = post.post 
     ul.append(p)
-
-
 }
+
+
 
