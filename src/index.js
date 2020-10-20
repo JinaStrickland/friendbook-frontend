b79af2ac59945d141e64cad189b5f8564cba0f61
JinaStrickland <Jina30@gmail.com>
2020-10-20 17:33:22 -0400

    posts and comments are up. posts likes is working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const postUrl = "http://localhost:3000/posts/"
+const postURL = "http://localhost:3000/posts/"
 const userURL = "http://localhost:3000/users/"
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,39 +12,71 @@ function fetchUser() {
 }
 
 function printPosts(user){
-    user.posts.forEach(post => displayPosts(post))
-
     const username = document.querySelector("#username")
-    username.innerText = `Welcome ${user.name}`
-}
+    username.innerText = user.name
+  
+    user.posts.forEach(post => {
+        const div = document.querySelector("div#posts")
 
-function displayPosts(post) {
-    const div = document.querySelector("div.posts")
-    const p = document.createElement("p")
-    p.innerHTML = post.post 
-    p.classList.add("post")
+        const newDiv = document.createElement("div")
+        newDiv.classList.add("panel-heading")
+        newDiv.innerHTML = username.innerText 
 
-    const postLikeBtn = document.createElement("button")
-    postLikeBtn.classList.add("p-like-btn")
-    postLikeBtn.innerHTML = `${post.likes} ♥`
-    div.append(p)
-    p.append(postLikeBtn)
+        const divBody = document.createElement("div")
+        divBody.classList.add("panel-body")
+        divBody.innerHTML = `posted at:  ${post.created_at}`
     
-    post.comments.forEach(comment => {
-            const ul = document.createElement("ul")
-            ul.classList.add("comments")
-            const li = document.createElement("li")
-            li.innerHTML = comment.comment
-            
+        const ul = document.createElement("ul")
+        ul.classList.add("list-group")
+        const li = document.createElement("li")
+        li.classList.add("list-group-item")
+        li.innerHTML = post.post 
+
+        const postLikeBtn = document.createElement("button")
+        postLikeBtn.classList.add("p-like-btn")
+        postLikeBtn.innerHTML = `${post.likes} ♥`
+        
+            postLikeBtn.addEventListener("click", () => {
+                let likes = post.likes
+                fetch(postURL + post.id, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        likes: ++post.likes
+                    })
+                })
+                .then(res => res.json())
+                .then(updatedLikes => {
+                    postLikeBtn.innerHTML = `${updatedLikes.likes} ♥` 
+                })
+            })
+
+        div.append(newDiv)
+        newDiv.append(divBody)
+        divBody.append(ul)
+        ul.append(li)
+        li.append(postLikeBtn)
+    
+        post.comments.forEach(comment => {
+            const extLi = document.querySelector("li.list-group-item")
+
+            const commentUl = document.createElement("ul")
+            commentUl.classList.add("list-group")
+            const commentLi = document.createElement("li")
+            commentLi.classList.add("list-group-item")
+            commentLi.innerHTML = comment.comment 
+
             const commentLikeBtn = document.createElement("button")
             commentLikeBtn.classList.add("c-like-btn")
             commentLikeBtn.innerHTML = `${comment.likes} ♥`
-            li.append(commentLikeBtn)
-            ul.append(li)
-            p.append(ul)
 
+            commentLi.append(commentLikeBtn)
+            commentUl.append(commentLi)
+            extLi.append(commentUl)
+        })
     })
-    
 }
 
 
